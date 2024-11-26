8f2642eb705879c6e2ca142990019b6e0177cfee
h2kyaw <mr.kiddomonster@gmail.com>
2024-11-27 02:20:45 +0630

    Added: Bored.

--- plugins/extra.py
+++ plugins/extra.py
@@ -1,3 +1,5 @@
+import requests
+
 from pyrogram import filters
 from TheApi import api
 from MPXMusic import app
@@ -9,10 +11,42 @@ async def advice(_, message):
     res = await api.get_advice()
     await A.edit(res)
 
+@app.on_message(filters.command("joke"))
+async def get_joke(_, message):
+    response = requests.get(
+        "https://v2.jokeapi.dev/joke/Any?blacklistFlags=political"
+    )
+    r = response.json()
+    joke_text = r["setup"]
+    await message.reply_text(joke_text)
 
+bored_api_url = "https://apis.scrimba.com/bored/api/activity"
+
+
+@app.on_message(filters.command("bored", prefixes="/"))
+async def bored_command(client, message):
+    response = requests.get(bored_api_url)
+    if response.status_code == 200:
+        data = response.json()
+        activity = data.get("activity")
+        if activity:
+            await message.reply(f"𝗙𝗲𝗲𝗹𝗶𝗻𝗴 𝗯𝗼𝗿𝗲𝗱? 𝗛𝗼𝘄 𝗮𝗯𝗼𝘂𝘁:\n\n {activity}")
+        else:
+            await message.reply("Nᴏ ᴀᴄᴛɪᴠɪᴛʏ ғᴏᴜɴᴅ.")
+    else:
+        await message.reply("Fᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴀᴄᴛɪᴠɪᴛʏ.")
+        
 __MODULE__ = "Exᴛʀᴀ"
 __HELP__ = """
 /advice - Gᴇᴛ ʀᴀɴᴅᴏᴍ ᴀᴅᴠɪᴄᴇ.
-/bored - Gᴇᴛs ʀᴀɴᴅᴏᴍ ᴀᴄᴛɪᴠɪᴛʏ ɪғ ʏᴏᴜ'ʀᴇ ʙᴏʀᴇᴅ.
-/quiz - ᴛᴏ ɢᴇᴛ ᴀɴ ʀᴀɴᴅᴏᴍ ǫᴜɪᴢ.
+/bored - Gᴇᴛs ʀᴀɴᴅᴏᴍ ᴀᴄᴛɪᴠɪᴛʏ.
+/joke - Gᴇᴛs ʀᴀɴᴅᴏᴍ Jᴏᴋᴇs.
+/quiz - Gᴇᴛs ʀᴀɴᴅᴏᴍ ǫᴜɪᴢ.
 """
+
+
+
+
+
+
+
